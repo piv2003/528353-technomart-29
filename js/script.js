4a8abfc5e71d8bbaa8a15b4c0bf341bfc52c5ee5
piv2003 <piv2003@gmail.com>
2020-10-25 22:04:48 +0200

    Работа над окном - "Напишите нам"

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
- // *******************************КАРТА START***************************
+// ***************************Окно "Товар добавлен в корзину" START***************************
   let buy = document.querySelectorAll(".buy");
   let ItemAddedToBasket = document.querySelector(".item-added-to-basket")
   let icon_close = document.querySelector(".button-close-basket")
@@ -15,7 +15,9 @@
     ItemAddedToBasket.classList.remove("open")
     ItemAddedToBasket.classList.toggle("close")
   });
+// ***************************Окно "Товар добавлен в корзину" END********
 
+// *******************************КАРТА START***************************
   let link = document.querySelector(".main-contacts-map");
   let popup = document.querySelector(".popup");
   let iconclose = document.querySelector(".popup_close");
@@ -38,9 +40,42 @@
     }
   }
   });
+// *******************************КАРТА  END***************************
 
- // *******************************КАРТА  END***************************
+// ***************************Окно "Напишите нам" START***************************
+const lostlink = document.querySelector(".contacts-gotlost");
+const loginPopup = document.querySelector(".modal-login");
+const loginClose = loginPopup.querySelector(".modal-close");
+const popup_got_lost = document.querySelector(".modal-login")
+const login = popup_got_lost.querySelector("[name=name]");
+const password = popup_got_lost.querySelector("[name=email]");
+const textLetter = document.querySelector(".text_letter-textarea")
+const button = document.querySelector(".button")
 
- // ***************************Окно "Товар добавлен в корзину" START***************************
+lostlink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.add("modal-show");
+  popup_got_lost.classList.remove("modal-error");
+});
 
-// ***************************Окно "Товар добавлен в корзину" END***************************
+loginClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.remove("modal-show");
+  popup_got_lost.classList.remove("modal-error");
+});
+
+button.addEventListener("click", function (evt) {
+  if (!login.value || !password.value || !textLetter.value) {
+   evt.preventDefault();
+   popup_got_lost.classList.remove("modal-error");
+   popup_got_lost.offsetWidth = popup_got_lost.offsetWidth;
+   popup_got_lost.classList.add("modal-error");
+   }
+});
+  window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    loginPopup.classList.remove("modal-show");
+  }
+  });
+// ***************************Окно "Напишите нам" END***************************
+
